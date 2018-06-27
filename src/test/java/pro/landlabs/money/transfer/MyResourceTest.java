c8319a032f51d28ca0527609e5af6206714c54e4
Alexander Land <landlab@gmail.com>
2018-06-27 12:10:06 +0300

    initial jersey webservice skeleton, maven build

---
+++ src/test/java/pro/landlabs/money/transfer/MyResourceTest.java
@@ -0,0 +1,39 @@
+package pro.landlabs.money.transfer;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+import static org.junit.Assert.assertEquals;
+
+public class MyResourceTest {
+
+    private HttpServer server;
+    private WebTarget target;
+
+    @Before
+    public void setUp() {
+        server = Main.startServer();
+
+        Client c = ClientBuilder.newClient();
+
+        target = c.target(Main.BASE_URI);
+    }
+
+    @After
+    public void tearDown() {
+        server.shutdownNow();
+    }
+
+    @Test
+    public void testGetIt() {
+        String responseMsg = target.path("myresource").request().get(String.class);
+        assertEquals("Got it!", responseMsg);
+    }
+
+}

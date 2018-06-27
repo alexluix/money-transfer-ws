58655f2adaf0971d27f89ba65b8d400abbf9101b
Alexander Land <landlab@gmail.com>
2018-06-27 12:45:46 +0300

    ws returns list of accounts

--- src/main/java/pro/landlabs/money/transfer/ws/AccountsResource.java
+++ src/main/java/pro/landlabs/money/transfer/ws/AccountsResource.java
@@ -1,5 +1,6 @@
 package pro.landlabs.money.transfer.ws;
 
+import com.google.common.collect.Lists;
 import pro.landlabs.money.transfer.ws.value.Account;
 
 import javax.ws.rs.Consumes;
@@ -8,6 +9,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.math.BigDecimal;
+import java.util.List;
 
 @Path("accounts")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -17,9 +19,11 @@ public class AccountsResource {
     public static final int ACCOUNT_DEFAULT_ID = 1;
     public static final int ACCOUNT_DEFAULT_BALANCE = 150;
 
+    private final Account defaultAccount = new Account(ACCOUNT_DEFAULT_ID, new BigDecimal(ACCOUNT_DEFAULT_BALANCE));
+
     @GET
-    public Account getAccount() {
-        return new Account(ACCOUNT_DEFAULT_ID, new BigDecimal(ACCOUNT_DEFAULT_BALANCE));
+    public List<Account> getAccounts() {
+        return Lists.newArrayList(defaultAccount, defaultAccount);
     }
 
 }
--- src/test/java/pro/landlabs/money/transfer/ws/AccountsResourceTest.java
+++ src/test/java/pro/landlabs/money/transfer/ws/AccountsResourceTest.java
@@ -1,5 +1,6 @@
 package pro.landlabs.money.transfer.ws;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.util.HttpStatus;
 import org.glassfish.jersey.jackson.JacksonFeature;
@@ -13,13 +14,18 @@ import pro.landlabs.money.transfer.ws.value.Account;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
 public class AccountsResourceTest {
+
+    private final TypeReference accountsTypeRef = new TypeReference<List<Account>>() { };
+    private final GenericType<List<Account>> accountsGenericType = new GenericType<>(accountsTypeRef.getType());
 
     private HttpServer server;
     private WebTarget target;
@@ -41,14 +47,21 @@ public class AccountsResourceTest {
     }
 
     @Test
-    public void shouldReturnAccountJson() {
+    public void shouldReturnListOfAccounts() {
+        // when
         Response response = target.path("accounts").request().get();
+
+        // then
         assertThat(response.getStatus(), equalTo(HttpStatus.OK_200.getStatusCode()));
         assertThat(response.getMediaType(), equalTo(MediaType.APPLICATION_JSON_TYPE));
 
-        Account account = response.readEntity(Account.class);
-        assertThat(account.getId(), equalTo(AccountsResource.ACCOUNT_DEFAULT_ID));
-        assertThat(account.getBalance().intValue(), equalTo(AccountsResource.ACCOUNT_DEFAULT_BALANCE));
+        List<Account> accounts = response.readEntity(accountsGenericType);
+        assertThat(accounts.size(), equalTo(2));
+
+        for (Account account : accounts) {
+            assertThat(account.getId(), equalTo(AccountsResource.ACCOUNT_DEFAULT_ID));
+            assertThat(account.getBalance().intValue(), equalTo(AccountsResource.ACCOUNT_DEFAULT_BALANCE));
+        }
     }
 
 }

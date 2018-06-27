684f814e47f6a5b139fde4566ff2b521de1e9546
Alexander Land <landlab@gmail.com>
2018-06-27 14:02:39 +0300

    logging introduced

--- src/main/java/pro/landlabs/money/transfer/Application.java
+++ src/main/java/pro/landlabs/money/transfer/Application.java
@@ -4,18 +4,22 @@ import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
 
 public class Application {
 
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
     public static final String BASE_URI = "http://localhost:8080/";
 
     public static void main(String[] args) throws IOException {
         final HttpServer server = startServer();
 
-        System.out.println(String.format("Money Transfer Service started. Endpoints: "
+        logger.info(String.format("Money Transfer Service started. Endpoints: "
                 + "%sapplication.wadl\nHit enter to shutdown...", BASE_URI));
 
         System.in.read();

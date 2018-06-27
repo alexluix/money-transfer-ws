5b02928a4801b8ed4183d93adc6bbfb92d99c6b3
Alexander Land <landlab@gmail.com>
2018-06-27 13:49:43 +0300

    server welcome message

--- src/main/java/pro/landlabs/money/transfer/Application.java
+++ src/main/java/pro/landlabs/money/transfer/Application.java
@@ -12,6 +12,17 @@ public class Application {
 
     public static final String BASE_URI = "http://localhost:8080/";
 
+    public static void main(String[] args) throws IOException {
+        final HttpServer server = startServer();
+
+        System.out.println(String.format("Money Transfer Service started. Endpoints: "
+                + "%sapplication.wadl\nHit enter to shutdown...", BASE_URI));
+
+        System.in.read();
+
+        server.shutdownNow();
+    }
+
     public static HttpServer startServer() {
         final ResourceConfig rc = new ResourceConfig()
                 .packages("pro.landlabs.money.transfer")
@@ -20,17 +31,6 @@ public class Application {
                 .register(JacksonFeature.class);
 
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
-    }
-
-    public static void main(String[] args) throws IOException {
-        final HttpServer server = startServer();
-
-        System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
-
-        System.in.read();
-
-        server.shutdownNow();
     }
 
 }
